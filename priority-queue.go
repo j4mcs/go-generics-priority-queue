668b087234f60a5781ee0457272a974b1296846d
James McNeil <jmcneil@pivotal.io>
2020-06-28 12:54:51 +0100

    remove comments

--- priority-queue.go
+++ priority-queue.go
@@ -7,45 +7,24 @@ import (
 
 ///HEAP
 
-// The Interface type describes the requirements
-// for a type using the routines in this package.
-// Any type that implements it may be used as a
-// min-heap with the following invariants (established after
-// Init has been called or if the data is empty or sorted):
-//
-//	!h.Less(j, i) for 0 <= i < h.Len() and 2*i+1 <= j <= 2*i+2 and j < h.Len()
-//
-// Note that Push and Pop in this interface are for package heap's
-// implementation to call. To add and remove things from the heap,
-// use heap.Push and heap.Pop.
 type Interface(type T) interface {
 	sort.Interface
-	Push(T) // add x as element Len()
-	Pop() T   // remove and return element Len() - 1.
+	Push(T)
+	Pop() T
 }
 
-// Init establishes the heap invariants required by the other routines in this package.
-// Init is idempotent with respect to the heap invariants
-// and may be called whenever the heap invariants may have been invalidated.
-// The complexity is O(n) where n = h.Len().
 func Init(type T)(h Interface(T)) {
-	// heapify
 	n := h.Len()
 	for i := n/2 - 1; i >= 0; i-- {
 		down(h, i, n)
 	}
 }
 
-// Push pushes the element x onto the heap.
-// The complexity is O(log n) where n = h.Len().
 func Push(type T)(h Interface(T), x T) {
 	h.Push(x)
 	up(h, h.Len()-1)
 }
 
-// Pop removes and returns the minimum element (according to Less) from the heap.
-// The complexity is O(log n) where n = h.Len().
-// Pop is equivalent to Remove(h, 0).
 func Pop(type T)(h Interface(T)) T {
 	n := h.Len() - 1
 	h.Swap(0, n)
@@ -53,8 +32,6 @@ func Pop(type T)(h Interface(T)) T {
 	return h.Pop()
 }
 
-// Remove removes and returns the element at index i from the heap.
-// The complexity is O(log n) where n = h.Len().
 func Remove(type T)(h Interface(T), i int) T {
 	n := h.Len() - 1
 	if n != i {
@@ -66,10 +43,6 @@ func Remove(type T)(h Interface(T), i int) T {
 	return h.Pop()
 }
 
-// Fix re-establishes the heap ordering after the element at index i has changed its value.
-// Changing the value of the element at index i and then calling Fix is equivalent to,
-// but less expensive than, calling Remove(h, i) followed by a Push of the new value.
-// The complexity is O(log n) where n = h.Len().
 func Fix(type T)(h Interface(T), i int) {
 	if !down(h, i, h.Len()) {
 		up(h, i)
@@ -78,7 +51,7 @@ func Fix(type T)(h Interface(T), i int) {
 
 func up(type T)(h Interface(T), j int) {
 	for {
-		i := (j - 1) / 2 // parent
+		i := (j - 1) / 2
 		if i == j || !h.Less(j, i) {
 			break
 		}
@@ -91,12 +64,12 @@ func down(type T)(h Interface(T), i0, n int) bool {
 	i := i0
 	for {
 		j1 := 2*i + 1
-		if j1 >= n || j1 < 0 { // j1 < 0 after int overflow
+		if j1 >= n || j1 < 0 {
 			break
 		}
-		j := j1 // left child
+		j := j1
 		if j2 := j1 + 1; j2 < n && h.Less(j2, j1) {
-			j = j2 // = 2*i + 2  // right child
+			j = j2
 		}
 		if !h.Less(j, i) {
 			break
@@ -138,21 +111,16 @@ func (pq *PriorityQueue) Pop() *Item {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
+	old[n-1] = nil
 	*pq = old[0 : n-1]
 	return item
 }
 
-// This example creates a PriorityQueue with some items, adds and manipulates an item,
-// and then removes the items in priority order.
 func main() {
-	// Some items and their priorities.
 	items := map[string]int{
 		"banana": 3, "apple": 2, "pear": 4,
 	}
 
-	// Create a priority queue, put the items in it, and
-	// establish the priority queue (heap) invariants.
 	pq := make(PriorityQueue, len(items))
 	i := 0
 	for value, priority := range items {
@@ -164,14 +132,12 @@ func main() {
 	}
 	Init(*Item)(&pq)
 
-	// Insert a new item and then modify its priority.
 	item := &Item{
 		value:    "orange",
 		priority: 1,
 	}
 	Push(*Item)(&pq, item)
 
-	// Take the items out; they arrive in decreasing priority order.
 	for pq.Len() > 0 {
 		item := Pop(*Item)(&pq)
 		fmt.Printf("%.2d:%s ", item.priority, item.value)
